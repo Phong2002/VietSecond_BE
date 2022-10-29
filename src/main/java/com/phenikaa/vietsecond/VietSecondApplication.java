40ff3018cd9d928148bdef88daa50b194aebd784
phonglm2002 <phong.luminh02@gmail.com>
2022-10-29 19:02:19 +0700

    create project VietSecond

---
+++ src/main/java/com/phenikaa/vietsecond/VietSecondApplication.java
@@ -0,0 +1,13 @@
+package com.phenikaa.vietsecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VietSecondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VietSecondApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/phenikaa/vietsecond/VietSecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phenikaa.vietsecond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VietSecondApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
