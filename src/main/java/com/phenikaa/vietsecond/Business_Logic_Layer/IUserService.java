b509e67299ca858872495075c2c4854fa8119d21
phonglm2002 <phong.luminh02@gmail.com>
2022-10-30 14:16:06 +0700

    create register abstract method

--- src/main/java/com/phenikaa/vietsecond/Business_Logic_Layer/IUserService.java
+++ src/main/java/com/phenikaa/vietsecond/Business_Logic_Layer/IUserService.java
@@ -1,11 +1,13 @@
 package com.phenikaa.vietsecond.Business_Logic_Layer;
 
 import com.phenikaa.vietsecond.Entity.User;
+import com.phenikaa.vietsecond.Form.UserForm;
 import org.springframework.data.domain.Page;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface IUserService extends UserDetailsService {
-    Page<User> getAllUser();
+    Page<User> GetAllUser();
     Boolean ExistsByUsername(String username);
-    User findByUserId(Integer userId);
+    User FindByUserId(Integer userId);
+    void Register(UserForm userForm);
 }
