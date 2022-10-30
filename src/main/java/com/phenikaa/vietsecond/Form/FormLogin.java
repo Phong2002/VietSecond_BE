889dfb75f87cb1519e336c9248783ec53fda6b1a
phonglm2002 <phong.luminh02@gmail.com>
2022-10-30 13:57:53 +0700

    valid fiel

--- src/main/java/com/phenikaa/vietsecond/Form/FormLogin.java
+++ src/main/java/com/phenikaa/vietsecond/Form/FormLogin.java
@@ -2,8 +2,12 @@ package com.phenikaa.vietsecond.Form;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class FormLogin {
+    @NotBlank(message = "user name must not be empty !")
     private String username;
+    @NotBlank(message = "user name must not be empty !")
     private String password;
 }
