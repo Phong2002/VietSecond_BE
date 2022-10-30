2e8802eee16f428244c7aabb58b513d0be163ae9
phonglm2002 <phong.luminh02@gmail.com>
2022-10-30 14:23:07 +0700

    create existsByPhoneNumber method with JPA

--- src/main/java/com/phenikaa/vietsecond/Data_Access_Layer/UserRepository.java
+++ src/main/java/com/phenikaa/vietsecond/Data_Access_Layer/UserRepository.java
@@ -9,4 +9,5 @@ public interface UserRepository extends JpaRepository<User,Integer>,JpaSpecifica
     Optional<User> findByUsername(String username);
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
+    Boolean existsByPhoneNumber(String phoneNumber);
 }
