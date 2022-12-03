1a2e7487fbfebd79f3b3ac49db28d4b0a96ffb98
phong2002 <phongkingvn@gmail.com>
2022-12-03 13:36:30 +0700

    find by id

--- src/main/java/com/phenikaa/vietsecond/Data_Access_Layer/VietnameseProvincesRepository/WardRepository.java
+++ src/main/java/com/phenikaa/vietsecond/Data_Access_Layer/VietnameseProvincesRepository/WardRepository.java
@@ -5,7 +5,11 @@ import com.phenikaa.vietsecond.Entity.VietnameseProvinces.Ward;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface WardRepository extends JpaRepository<Ward, String> {
     List<Ward> findByDistrictCode(District district);
+
+    @Override
+    Optional<Ward> findById(String s);
 }
